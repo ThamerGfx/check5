30aa2e0bbc21059dde7f198766ee014fc12331a8
ThamerGfx <36084619+ThamerGfx@users.noreply.github.com>
2018-11-24 15:41:30 +0100

    Add files via upload

---
+++ grp.js
@@ -0,0 +1,54 @@
+import React, {Component} from "react";
+import myfont from './Courgette-Regular.ttf';
+import myfont1 from './JosefinSans-Bold.ttf';
+
+class Grp extends Component {
+    render(props) {
+        let group = {};
+        if (this.props.gender === "female"){
+            group = {
+                border:'3px solid #FFAC13',
+                
+            }
+        }
+        else if(this.props.gender === "male"){
+            group = {
+                border: '3px solid #295A85',
+                
+            }
+        }
+        const job = {
+            color: 'rgb(161, 159, 159)',
+            fontSize: '150%',
+            margin: '0%',
+            fontFamily: myfont1,
+            paddingBbottom: '10px'
+            }
+        const name = {
+            display: 'flex',
+            fontSize: '160%',
+            fontFamily: myfont,
+            color:'rgb(94, 94, 94)'
+          }
+        const sname = {
+            paddingLeft: '10px',
+            color:'rgb(65, 64, 64)'
+          }
+        return(
+            <div className="grp">
+                <img style={group} src={this.props.pic} alt=""/>
+                <div style={group}>
+                    <div style={name}>
+                        <h3>{this.props.fname}</h3>
+                        <h3><b style={sname}>{this.props.sname}</b></h3>
+                        <h6 >({this.props.gender})</h6>
+                    </div>
+                </div>
+                <h4 style = {job}>{this.props.job1}</h4>
+                <h4 style={job}>{this.props.job2}</h4>
+            </div>
+        );
+    }
+
+}
+export default Grp;
